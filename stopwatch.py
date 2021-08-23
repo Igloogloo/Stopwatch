4d279d94fde5f9fbeacc421e9734527828875741
Isaac Sheidlower <55815140+Igloogloo@users.noreply.github.com>
2021-08-23 18:15:26 -0400

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,18 @@
+
+import time
+
+class Stopwatch():
+    def __init__(self):
+        self.start_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def duration(self):
+        if self.start_time is None:
+            print("Call start() before duration.")
+            return 0
+        return time.time() - self.start_time
+
+    def reset(self):
+        self.start_time = time.time()
